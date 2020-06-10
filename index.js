f2303f1fb9467002387e9605d3a6879194dff5a1
furkansoyturk <24878062+furkansoyturk@users.noreply.github.com>
2020-06-10 16:19:21 +0300

    init todo app

---
+++ index.js
@@ -0,0 +1,20 @@
+const electron = require('electron');
+
+const { app, BrowserWindow, Menu } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+});
+
+const menuTemplate = [
+    {
+        label: 'file'
+    }
+]
